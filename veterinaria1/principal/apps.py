8374ca63034dbfbb98c0d7680954011f2ed5d478
majin <mmajin65@misena.edu.co>
2022-05-17 08:17:39 -0500

    one

--- veterinaria1/principal/apps.py
+++ veterinaria1/principal/apps.py
@@ -5,4 +5,5 @@ class PrincipalConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'principal'
 
-"#principal.apps.PrincipalConfig"
+
+##principal.apps.PrincipalConfig
